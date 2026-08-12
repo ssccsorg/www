87f153cbdbcd8d1e964b0a377e08967589c64b9c
Theo <theo@ssccs.org>
2026-08-12 21:04:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -231,7 +231,7 @@ export default function Home() {
       </p>
       <ul>
         <li>
-          <a href="https://github.com/ssccsorg/ssccs/tree/main/poc">Abstract PoCs</a>:
+          <a href="https://github.com/ssccsorg/ssccs/tree/main/poc">PoCs</a>:
           SSCCS is a paradigm, not a single implementation. Its abstract
           primitives are being proven in parallel across RTL, RISC-V assembly,
           and current-language PoCs before a reference compiler and runtime
